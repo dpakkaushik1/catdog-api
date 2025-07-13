dadb0fa9e50fa867b51d91f2aad4e8faa622ade5
Deepak Kaushik <deepakkaushik8919@gmail.com>
2025-07-13 16:49:57 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = load_model("cats_vs_dogs_model.keras")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No image uploaded'})
+
+    file = request.files['file']
+    img = image.load_img(file, target_size=(160, 160))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    pred = model.predict(img_array)[0][0]
+    label = 'Dog' if pred > 0.5 else 'Cat'
+    return jsonify({'prediction': label, 'confidence': float(pred)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
